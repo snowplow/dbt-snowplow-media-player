774fdbc83780111fa29dbb1f269f63a9ffc43e67
Ryan Hill <r.l.hill128@gmail.com>
2023-09-15 13:05:21 +0100

    Update license to SPAL

--- macros/snowplow_media_player_base_events_this_run/media_event_type_field.sql
+++ macros/snowplow_media_player_base_events_this_run/media_event_type_field.sql
@@ -1,3 +1,10 @@
+{#
+Copyright (c) 2022-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Personal and Academic License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Personal and Academic License Version 1.0.
+You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
+#}
+
 {% macro media_event_type_field(media_player_event_type, event_name) %}
   coalesce(
     {% if var("snowplow__enable_media_player_v1") -%}
